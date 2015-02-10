d2fe726fda4838688f00b5a277fc671280764300
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-02-10 17:46:58 -0500

    add state test

--- test/models/state_test.rb
+++ test/models/state_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "I know my state fixtues" do
+    assert_equal "North Carolina", states(:one).name
+    assert_equal "NC", states(:one).abbreviation
+    assert_equal "South Carolina", states(:two).name
+    assert_equal "SC", states(:two).abbreviation
+  end
 end
