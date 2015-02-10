6480abac4d79941f6b7ebc49c044000692d12b14
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-02-10 16:50:27 -0500

    add belongs_to to contact

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ActiveRecord::Base
+  has_many :numbers
+  has_many :email_addresses
+  has_many :addresses
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ActiveRecord::Base
+  has_and_belongs_to_many :groups
+  has_many :numbers
+  has_many :email_addresses
+  has_many :addresses
+  has_many :notes
 end
