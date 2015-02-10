8877c136c7056c42f286c9830ae55c1e83353bde
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-02-10 17:07:24 -0500

    PAUSE for associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,7 @@ class Contact < ActiveRecord::Base
   has_many :email_addresses
   has_many :addresses
   has_many :notes
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "primary_id"
+  belongs_to :relationships, class_name: "Relationship", foreign_key: "secondary_id"
 end
