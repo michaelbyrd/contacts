ca19ac6251a92920497bf2db5c0c099843e34b1b
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-02-10 17:31:33 -0500

    add numbers test

--- test/models/number_test.rb
+++ test/models/number_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class NumberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "I know my number fixtures" do
+    assert_equal 1234567, numbers(:one).number
+    assert_equal 1112222, numbers(:two).number
+    assert_equal 9876543, numbers(:three).number
+  end
+
+  test "Numbers belong_to a contact" do
+    assert_equal 1234567, contacts(:one).numbers.first.number
+    assert_equal 1112222, contacts(:two).numbers.first.number
+  end
+
+  test "A contact can have multiple numbers" do
+    assert_equal 1234567, contacts(:one).numbers.first.number
+    assert_equal 9876543, contacts(:one).numbers[1].number
+  end
 end
