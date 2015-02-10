a1854c5b31304a72e75d98311efa2e83112b2aa1
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-02-10 18:04:08 -0500

    add street_addresses test

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ActiveRecord::Base
+  has_many :street_addresses
   belongs_to :contact
   belongs_to :category
   belongs_to :state
--- test/models/street_address_test.rb
+++ test/models/street_address_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class StreetAddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "I know my street_addresses fixtures" do
+    assert_equal "Apartment 4", street_addresses(:one).line
+    assert_equal "42 wallabee way", street_addresses(:two).line
+    assert_equal 1, street_addresses(:one).line_number
+    assert_equal 2, street_addresses(:two).line_number
+  end
+
+  test "street_addresses belong_to addresses" do
+    assert_equal "Apartment 4", addresses(:one).street_addresses.first.line
+  end
 end
